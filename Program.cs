ef39a8db638b6241f39640b5f48af9c26ffee3d0
Suhyun Chun <89771577+suhyunChun@users.noreply.github.com>
2023-11-14 21:11:35 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,15 +2,15 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Dynamic;
 using System.Runtime.CompilerServices;
-​
+using System.Threading;
 internal class Program
 {
     private static Character player;
     private static List<Item> items = new List<Item>();
     private static Item item;
-​
+
     private static List<Item> shopItems = new List<Item>();
-​
+
     
     static void Main(string[] args)
     {
@@ -20,37 +20,42 @@ internal class Program
         // DisplayGameIntro();
         StartMenu();
     }
-​
+
     public static void AddItem(List<Item> items, string name, string desc, Boolean isUsed, Dictionary<string,int>stat){
         Item item = new Item(name,desc,isUsed,stat);
         items.Add(item);
-​
+
     }
     public static void AddShopItem(List<Item> shopItems, string name, string desc, Dictionary<string,int> stat, int cost){
         Item item = new Item(name,desc,stat,cost);
         shopItems.Add(item);
     }
-    public static void StartMenu(){
-        Console.Clear();
-        DisplayGameIntro();
-    }
+
     static void GameDataSetting()
     {
         // 캐릭터 정보 세팅
         player = new Character("Chad", "전사",1, 10, 5, 100, 1500);
-​
+
         // 아이템 정보 세팅
         AddItem(items,"무쇠갑옷","무쇠로 만들어져 튼튼한 갑옷입니다",true,new Dictionary<string,int>(){["Def"]=5});        
         AddItem(items,"낡은 검","쉽게 볼 수 있는 낡은 검입니다.",false,new Dictionary<string,int>(){["Atk"]=2});
-​
-​
+
+
         AddShopItem(shopItems, "수련자 갑옷","수련에 도움을 주는 갑옷입니다.",new Dictionary<string, int>(){["Def"]=5},1000);
         AddShopItem(shopItems, "무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다", new Dictionary<string, int>() { ["Def"] = 9 }, 1500);
         AddShopItem(shopItems, "스파르타의 갑옷", " 스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", new Dictionary<string, int>() { ["Def"] = 15 }, 3500);
         AddShopItem(shopItems, "낡은 검 ", "쉽게 볼 수 있는 낡은 검 입니다. ", new Dictionary<string, int>() { ["Atk"] = 2 }, 600);
         AddShopItem(shopItems, "청동 도끼 ", "어디선가 사용됐던거 같은 도끼입니다 ", new Dictionary<string, int>() { ["Atk"] = 5}, 1500);
         AddShopItem(shopItems, "스파르타의 창 ", "스파르타의 전사들이 사용했다는 전설의 창입니다 ", new Dictionary<string, int>() { ["Atk"] = 7 }, 3500);
-​
+
+    }
+
+
+    //Scene -----------------------------------
+    public static void StartMenu()
+    {
+        Console.Clear();
+        DisplayGameIntro();
     }
     public static void PrintStartLogo(){
         
@@ -103,7 +108,7 @@ internal class Program
             case 2:
 				DisplayInventory();			
                 break;
-​
+
             case 3:
                 DisplayShop();
                 break;
@@ -115,13 +120,13 @@ internal class Program
         Console.WriteLine("상점");
         Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
         Console.WriteLine();
-​
+
         Console.WriteLine("[보유 골드]");
         Console.WriteLine($"{player.Gold} G");
         Console.WriteLine();
-​
+
         Console.WriteLine("[아이템 목록]");
-​
+
         foreach (Item item in shopItems)
         {
             Console.Write($"- {item.Name}");
@@ -135,7 +140,7 @@ internal class Program
                 {
                     Console.Write($"| 방어력 + {i.Value} ");
                 }
-​
+
             }
             Console.Write($" | {item.Desc}");
             if (item.isSold)
@@ -147,11 +152,11 @@ internal class Program
                 Console.WriteLine($" | {item.Cost} G");
             }
         }
-​
+
         Console.WriteLine();
         Console.WriteLine("1. 아이템 구매");
         Console.WriteLine("0. 나가기");
-​
+
         int input = CheckValidInput(0, 1);
         switch (input)
         {
@@ -162,24 +167,24 @@ internal class Program
             case 1:
 				DisplayBuyProduct();			
                 break;
-​
+
         }
-​
-​
-​
+
+
+
     }
     static void DisplayBuyProduct(){
         Console.Clear();
         Console.WriteLine("상점 - 아이템 구매");
         Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
         Console.WriteLine();
-​
+
         Console.WriteLine("[보유 골드]");
         Console.WriteLine($"{player.Gold} G");
         Console.WriteLine();
-​
+
         Console.WriteLine("[아이템 목록]");
-​
+
         int idx = 1;
         foreach (Item item in shopItems)
         {
@@ -195,7 +200,7 @@ internal class Program
                 {
                     Console.Write($"| 방어력 + {i.Value} ");
                 }
-​
+
             }
             Console.Write($" | {item.Desc}");
             if (item.isSold)
@@ -208,21 +213,25 @@ internal class Program
             }
             idx += 1;
         }
-​
-​
-​
+
+
+
         Console.WriteLine();
         Console.WriteLine("0. 나가기");
-​
+
         int input = CheckValidInput(0, shopItems.Count);
-​
-        if (shopItems[input - 1].IsSold)
+
+        if (input >0 && shopItems[input - 1].isSold )
         {
             Console.WriteLine("이미 구매한 아이템입니다.");
+            Thread.Sleep(1000);
+            DisplayBuyProduct();
         }
-        else if (player.Gold < shopItems[input - 1].Cost)
+        else if (input >0 && player.Gold < shopItems[input - 1].Cost )
         {
             Console.WriteLine("Gold가 부족합니다");
+            Thread.Sleep(1000);
+            DisplayBuyProduct();
         }
         else
         {
@@ -242,14 +251,14 @@ internal class Program
                     }
             }
         }
-​
+
     }
     static void DisplayMyInfo()
     {
-​
+
         int totalAtk = 0;
         int totalDef = 0;
-​
+
         Console.Clear();
     
         foreach(Item item in items){
@@ -265,8 +274,8 @@ internal class Program
                 }
             }
         }   
-​
-​
+
+
         Console.WriteLine("상태보기");
         Console.WriteLine("캐릭터의 정보르 표시합니다.");
         Console.WriteLine();
@@ -294,11 +303,11 @@ internal class Program
             case 0:
                 DisplayGameIntro();
                 break;
-​
+
             
         }
     }
-​
+
     static void DisplayEquippedInventory(){
         int idx = 1;   
         Console.Clear();
@@ -325,14 +334,14 @@ internal class Program
                 if(i.Key == "Def"){
                     Console.Write($"| 방어력 + {i.Value} ");
                 }
-​
+
             }
             Console.Write($"|{item.Desc}\n");
     
-​
+
         }
         Console.WriteLine();
-​
+
         Console.WriteLine("0. 나가기");
         Console.WriteLine("원하시는 행동을 입력해주세요.");
         int input = CheckValidInput(0, idx-1);        
@@ -342,9 +351,9 @@ internal class Program
             items[input-1].setIsUsed();  
             //Console.WriteLine($"{input-1}->{items[input-1].Name}");
             DisplayEquippedInventory();
-​
+
         }
-​
+
     }
         
     
@@ -358,7 +367,7 @@ internal class Program
         Console.WriteLine("[아이템 목록]");
         foreach(Item item in items){
             
-​
+
        
             Console.Write("-");
             if(item.IsUsed){
@@ -372,16 +381,16 @@ internal class Program
                 if(i.Key == "Def"){
                     Console.Write($"| 방어력 + {i.Value} ");
                 }
-​
+
             }
             Console.Write($"|{item.Desc}\n");
-​
-​
+
+
         }
         Console.WriteLine();
         Console.WriteLine("1. 장착 관리");
         Console.WriteLine("0. 나가기");
-​
+
         int input = CheckValidInput(0, 1);
         switch (input)
         {
@@ -392,16 +401,19 @@ internal class Program
                 DisplayEquippedInventory();
                 break;
         }
-​
-​
+
+
     }
-    
+
+
+    //  -----------------------------------
+
     static int CheckValidInput(int min, int max)
     {
         while (true)
         {
             string input = Console.ReadLine();
-​
+
             bool parseSuccess = int.TryParse(input, out var ret);
             if (parseSuccess)
             {
@@ -412,9 +424,11 @@ internal class Program
             Console.WriteLine("잘못된 입력입니다.");
         }
     }
+
+
 }
-​
-​
+
+/// Class 
 public class Character
 {
     public string Name { get; }
@@ -424,7 +438,7 @@ public class Character
     public int Def { get; }
     public int Hp { get; }
     public int Gold { get; set; }
-​
+
     public Character(string name, string job, int level, int atk, int def, int hp, int gold)
     {
         Name = name;
@@ -435,10 +449,10 @@ public class Character
         Hp = hp; //체력
         Gold = gold;
     }
-​
-​
+
+
 }
-​
+
 public class Item{
     public String Name {get;}
     public String Desc {get;}
@@ -446,7 +460,7 @@ public class Item{
     public Boolean isSold {get; set;}
     public int Cost {get;}
     public Dictionary<string,int> Stats {get; set;}
-​
+
 // ex) {"Atk", 1}, {"Def",1} ... 
     public Item(string name, string desc, Boolean isUsed, Dictionary<string,int> stats){
         Name = name;
@@ -461,13 +475,13 @@ public class Item{
         Stats = stats;
         Cost = cost;
         isSold = false;
-​
+
     }
     
     public Boolean setIsUsed(){
        IsUsed = !IsUsed;
        return IsUsed;
-​
+
     }
     public Boolean setIsSold(){
         isSold = true;
